24c983e0a4d1ed739fe112fb5342b09de3e1f065
Tama Dioo <tamadio91@gmail.com>
2023-09-30 20:34:33 +0700

    Commit dan push hasil praktikum jobsheet 1

---
+++ Latihan09.java
@@ -0,0 +1,14 @@
+/**
+ * Latihan09
+ */
+public class Latihan09 {
+ 
+    public static void main(String[] args) {
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama        : Dio Andika Pradana Mulia Tama");
+        System.out.println("Absen       : 09");
+        System.out.println("Kelas       : 1B");
+        System.out.println("Prodi       : D-IV Teknik Informatika");
+        System.out.println("Jurusan     : Teknologi Informasi");
+    }
+}
---
+++ MyFirstJava09.java
@@ -0,0 +1,7 @@
+public class MyFirstJava09 {
+    public static void main (String[] args)  {
+
+        System.out.println("Nama Saya Dio Andika Pradana Mulia Tama");
+
+    }
+}
